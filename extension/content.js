66f3b72056bed5dafc8bc9b40486852ec9fb6ef7
Krish Goradia <krishgoradia06@gmail.com>
2026-02-09 13:46:08 +0530

    add remaining file

---
+++ extension/content.js
@@ -0,0 +1,58 @@
+let currentfieldkey = null;
+
+chrome.runtime.onMessage.addListener((msg)=>{
+    if(msg.type !== "START_SELECTION") return;
+    currentfieldkey = msg.fieldkey;
+    enableclickget();
+})
+
+function enableclickget(){
+    document.addEventListener("click",onelementclick,true);
+    document.addEventListener("mouseover",onhoveringelement,true);
+    document.addEventListener("mouseout",onunhoveringelement,true);
+}
+
+let previoushovered = null;
+function onhoveringelement(e){
+    if(previoushovered) previoushovered.style.outline = "";
+    e.target.style.outline = "2px solid yellow";
+    previoushovered = e.target;
+}
+function onunhoveringelement(e){
+    e.target.style.outline = "";    
+}
+function onelementclick(e){
+    e.preventDefault();
+    e.stopPropagation();
+    if(e.target) e.target.style.outline = "";
+    document.removeEventListener("click",onelementclick,true);
+    document.removeEventListener("mouseover",onhoveringelement,true);
+    document.removeEventListener("mouseout",onunhoveringelement,true);
+
+    const xpath = getxpath(e.target);
+    chrome.runtime.sendMessage({
+        type:"FIELD_SELECTED",
+        fieldkey: currentfieldkey,
+        xpath:xpath
+    });
+
+    currentfieldkey = null;
+}
+
+function getxpath(element){
+    if(element.id) return `//*[@id="${element.id}"]`;
+    if(element === document.body) return '/html/body';
+     
+    let cnt = 0;
+    const siblings = element.parentNode.childNodes;
+    for(let i = 0;i<siblings.length;i++){
+        const sib = siblings[i];
+        if(sib === element){
+            return getxpath(element.parentNode) + "/" + element.tagName.toLowerCase() + `[${cnt+1}]`;
+        }
+        if(sib.nodeType === 1 && sib.tagName === element.tagName){
+            cnt++;
+        }
+    } 
+    return null;
+}
---
+++ extension/sidepanel.js
@@ -0,0 +1,101 @@
+async function panelopen(){
+    const [tab] = await chrome.tabs.query({active:true,currentWindow:true});
+
+    chrome.runtime.sendMessage({
+        type:"PANEL_OPEN",
+        tabId: tab.id
+    });
+}
+
+panelopen();
+
+chrome.runtime.onMessage.addListener((msg)=>{
+    if(msg.type != "CONFERENCE_READY") return;
+
+    const {isNew,conf_id,existing_fields,meta} = msg;
+
+    window.currentConf = {
+        conf_id,
+        isNew,
+        existing_fields,
+        pending_fields : {},
+        meta
+    };
+
+    showInitialUI(isNew,existing_fields,meta);
+
+});
+
+function showInitialUI(isNew,existing_fields,meta){
+    if(!isNew){
+        for(let fieldkey in existing_fields){
+            const field = existing_fields[fieldkey]
+            showfieldUI(fieldkey,field.label,true);
+        }
+    }
+    else{
+        document.getElementById("field_container").innerHTML = "";
+    }
+    if(meta){
+        document.getElementById("conference_name").textContent = meta.name;
+    }
+}
+function showfieldUI(fieldkey,label,isDone){
+    const div = document.createElement("div");
+    div.id = `field-${fieldkey}`;
+    div.textContent = label + (isDone ? " DONE ✅" : "");
+    fieldContainer.appendChild(div);
+}
+const fieldSelect = document.getElementById("add_field");
+const fieldContainer = document.getElementById("field_container");
+const submitbtn = document.getElementById("submitbtn");
+
+fieldSelect.addEventListener("change", async(eve)=>{
+    const fieldkey = eve.target.value;
+    if(!fieldkey) return;
+    const {existing_fields,pending_fields} = window.currentConf;
+    if(existing_fields[fieldkey] || pending_fields[fieldkey]){
+        alert("This field has been added");
+        return;
+    }
+    pending_fields[fieldkey] = {selected:false};
+    addnewfieldtoUI(fieldkey);
+
+    const [tab] = await chrome.tabs.query({active:true,currentWindow:true});
+    chrome.tabs.sendMessage(tab.id,{
+        type: "START_SELECTION",
+        fieldkey
+    });
+    fieldSelect.value = "";
+
+})
+
+function addnewfieldtoUI(fieldkey){
+    const div = document.createElement("div");
+    div.id = `field-${fieldkey}`;
+    div.textContent = fieldkey + "(selecting in progress...)";
+    fieldContainer.appendChild(div);
+}
+
+chrome.runtime.onMessage.addListener((msg)=>{
+    if(msg.type!="FIELD_ADDED") return;
+    const {fieldkey} = msg;
+    window.currentConf.pending_fields[fieldkey].selected=true;
+    const div = document.getElementById(`field-${fieldkey}`);
+    if(div) div.textContent = fieldkey + "DONE ✅";
+
+    checkreadyforsubmit();
+})
+
+function checkreadyforsubmit(){
+    const {pending_fields} = window.currentConf;
+    const done = Object.values(pending_fields).every(f=> f.selected == true);
+    submitbtn.disabled = !done;
+}
+
+submitbtn.addEventListener("click",()=>{
+    chrome.runtime.sendMessage({
+        type: "SUBMIT_CONFERENCE",
+        conf_id: window.currentConf.conf_id
+    });
+});
